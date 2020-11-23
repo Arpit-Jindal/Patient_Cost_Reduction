8d4112971095d66b274c947343e3f9dbab1a0d73
Arpit Jindal <arpitjindaldtu@gmail.com>
2020-11-23 20:50:32 +0530

    added patient IDs

--- frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -7,12 +7,16 @@ import { Component, OnInit } from "@angular/core";
 })
 export class SidebarComponent implements OnInit {
   patient_list = [
-    "5b891358-1bb3-4bbf-b8a6-a73fbe58efe7",
-    "10339b10-3cd1-4ac3-ac13-ec26728cb592",
-    "72c0b9ce-7aa4-430b-aaff-bd0ce7846e55",
-    "b58731cc-2d8b-4c2d-b327-4cab771af3ef",
-    "cfee79fc-df05-476e-b274-43e09ea345db",
-    "ad2e9916-4979-40fc-a8c0-68651a0cb5a6",
+    "128d5c93-dfce-49a1-8d08-e9b24abcb4db",
+    "0042862c-9889-4a2e-b782-fac1e540ecb4",
+    "cae10920-f977-48b4-a0d3-4d70ad561fd1",
+    "756e459c-b53e-4120-b427-a80a5caebc06",
+    "33454fc9-b2ea-407a-9a9d-64897cf44093",
+    "be33d994-f841-40a4-9410-66555cf90fed",
+    "bb30c9d3-913c-49ce-8cd9-75405f7a19e9",
+    "b1e9b0b9-da6e-4f68-b603-bd896a50ca86",
+    "0149d553-f571-4e99-867e-fcb9625d07c2",
+    "5c06120a-9af5-4204-951b-7a8bfc465df3",
   ];
   constructor() {}
 
